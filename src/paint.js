4d86c5e80ed3093b88d89681667ebe96c463bddd
Kiril Vatev <vatev.1@gmail.com>
2017-12-17 14:40:54 -0500

    a whole lot of code rearranging, making it possible to select a color on stopped video

--- src/paint.js
+++ src/paint.js
@@ -3,127 +3,148 @@
 
 (function (register) {
   var NAME = 'paint';
+  var EVENTS;
 
   var canvas = document.querySelector('canvas');
+  var context = canvas.getContext('2d');
+  var painting = false;
+  var lastFrame, canvasWidth, canvasHeight;
+
+  // define patch size and location
+  var patchSize = 11;
+  var patchX, patchY;
+
+  function drawImageData(data) {
+    context.putImageData(data, 0, 0);
+  }
+
+  function getPatch() {
+    var offset = Math.floor(patchSize / 2);
+    var x = patchX - offset;
+    var y = patchY - offset;
+
+    if (x < 1) {
+      x = 1;
+    }
+
+    if (y < 1) {
+      y = 1;
+    }
+
+    if (x + patchSize > canvasWidth - 1) {
+      x = canvasWidth - 1 - patchSize;
+    }
+
+    if (y + patchSize > canvasHeight - 1) {
+      y = canvasHeight - 1 - patchSize;
+    }
+
+    return { x: x, y: y };
+  }
+
+  function captureColor() {
+    var patch = getPatch();
+
+    var pixels = [].slice.call(context.getImageData(patch.x, patch.y, patchSize, patchSize).data);
+    var colors = [];
+
+    while (pixels.length) {
+      // pixels are an array of rgba values
+      colors.push(pixels.splice(0, 4));
+    }
+
+    var average = colors.reduce(function (a, b) {
+      return [
+        a[0] + b[0],
+        a[1] + b[1],
+        a[2] + b[2],
+        a[3] + b[3]
+      ];
+    }).map(function (c) {
+      return Math.floor(c / colors.length);
+    });
+
+    // draw rectangle around the selected area
+    context.beginPath();
+    context.lineWidth = '1';
+    context.strokeStyle = '#e5e5e5';
+    context.rect(patch.x - 1, patch.y - 1, patchSize + 2, patchSize + 2);
+    context.stroke();
+
+    return {
+      r: average[0],
+      g: average[1],
+      b: average[2]
+    };
+  }
+
+  function drawColor() {
+    var color = captureColor();
+    EVENTS.emit('color-change', { color: color });
+  }
+
+  function onCanvasClick(ev) {
+    // update the patch center, if the events support it
+    patchX = ev.offsetX || ev.layerX || patchX;
+    patchY = ev.offsetY || ev.layerY || patchY;
+
+    if (!painting && lastFrame) {
+      drawImageData(lastFrame);
+      drawColor();
+    }
+  }
 
   function init() {
     canvas.classList.remove('hide');
+
+    canvasWidth = canvas.clientWidth;
+    canvasHeight = canvas.clientHeight;
+
+    // set the actual width and height of the canvas,
+    // because apparently it's not inherited from the DOM
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+
+    patchX = Math.floor(canvasWidth / 2);
+    patchY = Math.floor(canvasHeight / 2);
+
+    canvas.addEventListener('click', onCanvasClick);
   }
 
   function destroy() {
     canvas.classList.add('hide');
+    canvas.removeEventListener('click', onCanvasClick);
+
+    lastFrame = null;
+    painting = false;
   }
 
-  function paintVideo(video, events) {
-    var painting = true;
+  function paintVideo(video) {
 
     return new Promise(function (resolve, reject) {
+      painting = true;
 
       var vw = video.videoWidth;
       var vh = video.videoHeight;
-      var cw = canvas.clientWidth;
-      var ch = canvas.clientHeight;
 
-      // set the actual width and height of the canvas,
-      // because apparently it's not inherited from the DOM
-      canvas.width = cw;
-      canvas.height = ch;
-
-      var vidX = (vw - cw) / 2;
-      var vidY = (vh - ch) / 2;
-
-      // define patch size and location
-      var patchSize = 11;
-      var patchX = Math.floor(cw / 2);
-      var patchY = Math.floor(ch / 2);
-
-      var context = canvas.getContext('2d');
-
-      function getPatch() {
-        var offset = Math.floor(patchSize / 2);
-        var x = patchX - offset;
-        var y = patchY - offset;
-
-        if (x < 1) {
-          x = 1;
-        }
-
-        if (y < 1) {
-          y = 1;
-        }
-
-        if (x + patchSize > cw - 1) {
-          x = cw - 1 - patchSize;
-        }
-
-        if (y + patchSize > ch - 1) {
-          y = ch - 1 - patchSize;
-        }
-
-        return { x: x, y: y };
-      }
-
-      function captureColor() {
-        var patch = getPatch();
-
-        var pixels = [].slice.call(context.getImageData(patch.x, patch.y, patchSize, patchSize).data);
-        var colors = [];
-
-        while (pixels.length) {
-          // pixels are an array of rgba values
-          colors.push(pixels.splice(0, 4));
-        }
-
-        var average = colors.reduce(function (a, b) {
-          return [
-            a[0] + b[0],
-            a[1] + b[1],
-            a[2] + b[2],
-            a[3] + b[3]
-          ];
-        }).map(function (c) {
-          return Math.floor(c / colors.length);
-        });
-
-        // draw rectangle around the selected area
-        context.beginPath();
-        context.lineWidth = '1';
-        context.strokeStyle = '#e5e5e5';
-        context.rect(patch.x - 1, patch.y - 1, patchSize + 2, patchSize + 2);
-        context.stroke();
-
-        return {
-          r: average[0],
-          g: average[1],
-          b: average[2]
-        };
-      }
+      var vidX = (vw - canvasWidth) / 2;
+      var vidY = (vh - canvasHeight) / 2;
 
       function drawContext() {
-        context.drawImage(video, vidX, vidY, cw, ch, 0, 0, cw, ch);
-      }
-
-      function drawColor() {
-        var color = captureColor();
-        events.emit('color-change', { color: color });
-      }
-
-      function onCanvasClick(ev) {
-        // update the patch center, if the events support it
-        patchX = ev.offsetX || ev.layerX || patchX;
-        patchY = ev.offsetY || ev.layerY || patchY;
+        context.drawImage(video, vidX, vidY, canvasWidth, canvasHeight, 0, 0, canvasWidth, canvasHeight);
       }
 
       function onStopVideo() {
-        events.off('stop-video', onStopVideo);
-        canvas.removeEventListener('click', onCanvasClick);
+        // draw one last frame and capture it
+        drawContext();
+        lastFrame = context.getImageData(0, 0, canvasWidth, canvasHeight);
+
+        EVENTS.off('stop-video', onStopVideo);
 
         painting = false;
       }
 
-      events.on('stop-video', onStopVideo);
-
-      canvas.addEventListener('click', onCanvasClick);
+      EVENTS.on('stop-video', onStopVideo);
 
       (function paintFrame () {
         if (!painting) {
@@ -143,10 +164,11 @@
 
   register(NAME, function () {
     var context = this;
+    EVENTS = context.events;
 
     context.events.on('video-playing', function (video) {
       init();
-      paintVideo(video, context.events);
+      paintVideo(video);
     });
 
     return destroy;
